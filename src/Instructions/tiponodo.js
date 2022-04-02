39a0899787ca74e397f251334e8d7de4f6b8a9b9
Alvaro-SP <socop2412@gmail.com>
2022-04-02 17:02:53 -0600

    add new commit

--- src/Instructions/tiponodo.js
+++ src/Instructions/tiponodo.js
@@ -95,41 +95,194 @@ var Tipo = /** @class */ (function () {
         this.tipo = tipo;
     }
     Tipo.prototype.toString = function () {
-        if (this.tipo === tipos.STRING) {
-            return 'string';
+        if (this.tipo === tipos.DECLARACION) {
+            return 'declaracion';
         }
-        else if (this.tipo === tipos.ENTERO) {
-            return 'entero';
+else if (this.tipo === tipos.ASIGNACION) {
+            return 'asignacion';
         }
-        else if (this.tipo === tipos.DECIMAL) {
+else if (this.tipo === tipos.DECIMAL) {
             return 'decimal';
         }
-        else if (this.tipo === tipos.BOOLEANO) {
-            return 'boolean';
-        }
-        else if (this.tipo === tipos.CARACTER) {
+else if (this.tipo === tipos.CARACTER) {
             return 'caracter';
         }
-        else if (this.tipo === tipos.VARIABLE) {
+else if (this.tipo === tipos.STRING) {
+            return 'string';
+        }
+else if (this.tipo === tipos.BOOLEAN) {
+            return 'boolean';
+        }
+else if (this.tipo === tipos.VECTOR) {
+            return 'vector';
+        }
+else if (this.tipo === tipos.VOID) {
+            return 'void';
+        }
+else if (this.tipo === tipos.METODO) {
+            return 'metodo';
+        }
+else if (this.tipo === tipos.FUNCION) {
+            return 'funcion';
+        }
+else if (this.tipo === tipos.VARIABLE) {
+            return 'variable';
+        }
+else if (this.tipo === tipos.EXPRESION) {
+            return 'expresion';
+        }
+else if (this.tipo === tipos.ARITMETICO) {
+            return 'aritmetico';
+        }
+else if (this.tipo === tipos.SUMA) {
+            return 'suma';
+        }
+else if (this.tipo === tipos.UNITARIA) {
+            return 'unitaria';
+        }
+else if (this.tipo === tipos.PRINT) {
+            return 'print';
+        }
+else if (this.tipo === tipos.MULTIPLICACION) {
+            return 'multiplicacion';
+        }
+else if (this.tipo === tipos.DIVISION) {
+            return 'division';
+        }
+else if (this.tipo === tipos.POTENCIA) {
+            return 'potencia';
+        }
+else if (this.tipo === tipos.MODULO) {
+            return 'modulo';
+        }
+else if (this.tipo === tipos.INT) {
+            return 'int';
+        }
+else if (this.tipo === tipos.RELACIONAL) {
+            return 'relacional';
+        }
+else if (this.tipo === tipos.MAYOR) {
+            return 'mayor';
+        }
+else if (this.tipo === tipos.MENOR) {
+            return 'menor';
+        }
+else if (this.tipo === tipos.MAYORIGUAL) {
+            return 'mayorigual';
+        }
+else if (this.tipo === tipos.MENORIGUAL) {
+            return 'menorigual';
+        }
+else if (this.tipo === tipos.IGUAL) {
+            return 'igual';
+        }
+else if (this.tipo === tipos.NEGACION) {
+            return 'negacion';
+        }
+else if (this.tipo === tipos. LOGICO) {
+            return 'logico';
+        }
+else if (this.tipo === tipos. OR) {
+            return 'or';
+        }
+else if (this.tipo === tipos. AND) {
+            return 'and';
+        }
+else if (this.tipo === tipos. NOT) {
+            return 'not';
+        }
+else if (this.tipo === tipos. DIFERENTE) {
+            return 'diferente';
+        }
+else if (this.tipo === tipos. TERNARIO) {
+            return 'ternario';
+        }
+else if (this.tipo === tipos. DECREMENTO) {
+            return 'decremento';
+        }
+else if (this.tipo === tipos. INCREMENTO) {
+            return 'incremento';
+        }
+else if (this.tipo === tipos. LLAMADA) {
+            return 'llamada';
+        }
+else if (this.tipo === tipos.IF) {
             return 'Variable';
         }
-        else if (this.tipo === tipos.METODO) {
-            return 'Metodo';
+else if (this.tipo === tipos.SWITCH) {
+            return 'switch';
         }
-        else if (this.tipo === tipos.FUNCION) {
-            return 'Funcion';
+else if (this.tipo === tipos.WHILE) {
+            return 'while';
         }
-        else if (this.tipo === tipos.VOID) {
-            return 'Void';
+else if (this.tipo === tipos.DOWHILE) {
+            return 'dowhile';
         }
-        else if (this.tipo === tipos.VECTOR) {
-            return 'Vector';
+else if (this.tipo === tipos.FOR) {
+            return 'for';
         }
-        else if (this.tipo === tipos.LIST) {
-            return 'List';
+else if (this.tipo === tipos.CONTINUE) {
+            return 'continue';
         }
-        else if (this.tipo === tipos.VALOR) {
-            return 'Valor';
+else if (this.tipo === tipos.RETURN) {
+            return 'return';
+        }
+else if (this.tipo === tipos.BREAK) {
+            return 'break';
+        }
+else if (this.tipo === tipos. LENGTH) {
+            return 'length';
+        }
+else if (this.tipo === tipos. TOSTRING) {
+            return 'tostring';
+        }
+else if (this.tipo === tipos. TYPEOF) {
+            return 'typeof';
+        }
+else if (this.tipo === tipos. ROUND) {
+            return 'round';
+        }
+else if (this.tipo === tipos. TRUNCATE) {
+            return 'truncate';
+        }
+else if (this.tipo === tipos. TOUPPER) {
+            return 'toupper';
+        }
+else if (this.tipo === tipos. TOLOWER) {
+            return 'tolower';
+        }
+else if (this.tipo === tipos.BLOQUE) {
+            return 'bloque';
+        }
+else if (this.tipo === tipos.PARAMETRO) {
+            return 'parametro';
+        }
+else if (this.tipo === tipos.CONTROL) {
+            return 'control';
+        }
+else if (this.tipo === tipos.ERROR) {
+            return 'error';
+        }
+else if (this.tipo === tipos.SEMANTICO) {
+            return 'semantico';
+        }
+else if (this.tipo === tipos.SINTACTICO) {
+            return 'sintactico';
+        }
+else if (this.tipo === tipos.LEXICO) {
+            return 'lexico';
+        }
+else if (this.tipo === tipos.LIST) {
+            return 'list';
+        }
+else if (this.tipo === tipos.VALOR) {
+            return 'valor';
+        }
+else if (this.tipo === tipos.CASTEO) {
+            return 'casteo';
+        }
+else if (this.tipo === tipos.NATIVA) {
+            return 'nativa';
         }
     };
     return Tipo;
